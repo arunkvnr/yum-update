73245b44240fe32ad6a282c263d8c0ccf7c62923
arunkvnr <41189465+arunkvnr@users.noreply.github.com>
2020-12-23 15:09:46 +0530

    Update yumupdate.sh

--- yumupdate.sh
+++ yumupdate.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-yum update -y 
-
 yum install -y nginx
+
+mkdir -p /var/www/html/
+touch index.html
 
 # Set the home page.
 echo "<html><body><h2>Welcome to Azure! My name is ARUN .</h2></body></html>" | sudo tee -a /var/www/html/index.html
