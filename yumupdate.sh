4e785f18025fab25859b225a7073d0143357354a
arunkvnr <41189465+arunkvnr@users.noreply.github.com>
2020-12-23 15:39:38 +0530

    Update yumupdate.sh

--- yumupdate.sh
+++ yumupdate.sh
@@ -2,8 +2,12 @@
 
 yum install -y nginx
 
-mkdir -p /var/www/html/
-touch index.html
+firewall-cmd --permanent --add-port={80/tcp,443/tcp}
+firewall-cmd --reload
+
+systemctl enable nginx
+
+systemctl start nginx
 
 # Set the home page.
-echo "<html><body><h2>Welcome to Azure! My name is ARUN .</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><body><h2>Welcome to Azure! My name is ARUN .</h2></body></html>" | sudo tee -a /usr/share/nginx/html
